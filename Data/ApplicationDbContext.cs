7c33266156c52576aacb90add66422168472b4a8
destinygit <vhutshilo96@gmail.com>
2020-11-28 20:33:07 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CCbaWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCbaWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
---
+++ Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,22 @@
+﻿using CCbaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCbaWeb.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        //Add all methods that will perform crud operation
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int nationalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+
+    }
+}
